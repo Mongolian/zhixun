a256b526d6214043786f5e47b4b70264bd4fad86
zlz2011124@gmail.com <zlz2011124@gmail.com>
2013-07-10 22:57:53 +0800

    *

---
+++ src/com/iory/zhixun/data/NewsKind.java
@@ -0,0 +1,14 @@
+package com.iory.zhixun.data;
+
+public class NewsKind {
+	
+	public int id ;
+	public String kindName ;
+	public int iconId;
+
+	public NewsKind(int id,String kind,int iconId){
+	this.id = id;
+	this.kindName = kind;
+	this.iconId = iconId;
+	}
+}
