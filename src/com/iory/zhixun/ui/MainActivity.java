e1d820a47ceee0df074fc160a2dd1cfb53742679
zlz <zlz2011124@gmail.com>
2013-07-20 00:42:49 +0800

    去掉了底部的滑动

--- src/com/iory/zhixun/ui/MainActivity.java
+++ src/com/iory/zhixun/ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.iory.zhixun.ui;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.iory.zhixun.R;
 import com.iory.zhixun.fragments.ColorFragment;
 import com.iory.zhixun.slidingmenu.lib.SlidingMenu;
@@ -17,8 +19,33 @@ public class MainActivity extends BaseActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getSlidingMenu().setMode(SlidingMenu.LEFT_RIGHT);
-		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-		
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+
+        getSlidingMenu().setOnClosedListener(new SlidingMenu.OnClosedListener() {
+            @Override
+            public void onClosed() {
+                Toast.makeText(MainActivity.this, "close", 0).show();
+            }
+        });
+        getSlidingMenu().setOnOpenListener(new SlidingMenu.OnOpenListener() {
+            @Override
+            public void onOpen() {
+                Toast.makeText(MainActivity.this, "open", 0).show();
+            }
+        });
+        getSlidingMenu().setOnOpenedListener(new SlidingMenu.OnOpenedListener() {
+            @Override
+            public void onOpened() {
+                Toast.makeText(MainActivity.this, "Opened", 0).show();
+            }
+        });
+        getSlidingMenu().setOnClosedListener(new SlidingMenu.OnClosedListener() {
+            @Override
+            public void onClosed() {
+                Toast.makeText(MainActivity.this, "Closed", 0).show();
+            }
+        });
+
 		setContentView(R.layout.content_frame);
 		getSupportFragmentManager()
 		.beginTransaction()
